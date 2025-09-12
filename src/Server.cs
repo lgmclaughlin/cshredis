1a260678ddc88a72884e3ff78427dd6a09b35a89
lgmclaughlin <lgmclaughlin905@gmail.com>
2025-09-12 14:46:52 -0700

    receive message through socket and respond

--- src/Server.cs
+++ src/Server.cs
@@ -1,6 +1,25 @@
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 TcpListener server = new TcpListener(IPAddress.Any, 6379);
-server.Start();
-server.AcceptSocket(); // wait for client
+
+try {
+	server.Start();
+	Socket clientSocket = server.AcceptSocket(); 
+
+	using (NetworkStream stream = new NetworkStream(clientSocket)) {
+		byte[] buffer = new byte[1024];
+		int bytesRead = stream.Read(buffer, 0, buffer.Length);
+		string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+		Console.WriteLine($"Received: {message}");
+
+		var response = "+PONG\r\n";
+		byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+		stream.Write(responseBytes, 0, responseBytes.Length);
+	}
+}
+finally
+{
+	server.Stop();
+}
