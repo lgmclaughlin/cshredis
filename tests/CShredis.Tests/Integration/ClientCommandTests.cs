886f82853ee79045e72a98ec95651fbe9528f9b7
lgmclaughlin <11427888+lgmclaughlin@users.noreply.github.com>
2025-10-09 00:29:07 -0700

    Adding simple integration testing.

---
+++ tests/CShredis.Tests/Integration/ClientCommandTests.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Text;
+using System.Threading;
+using Xunit;
+using System.Net.Sockets;
+using CShredis.Core;
+
+namespace CShredis.Tests
+{
+	public class ClientCommandTests : IDisposable
+	{
+		private const int CLIENT_PORT = 6379;
+
+		private const string CLIENT_HOSTNAME = "localhost";
+
+		private Server _server;
+
+		private Thread _serverThread;
+
+		public ClientCommandTests()
+		{
+			_server = new Server();
+			_serverThread = new Thread(_server.Start);
+			_serverThread.Start();
+
+			Thread.Sleep(200);
+		}
+
+		public void Dispose()
+		{
+			_server.Stop();
+			_serverThread.Join();
+		}
+
+		[Fact]
+		public void PingCommand_ReturnsPong()
+		{
+			var stream = GetNewClientStream();
+
+			var request = "*1\r\n$4\r\nPING\r\n";
+			
+			var response = SendRequestAndGetResponse(request, stream);
+			Assert.Equal("+PONG\r\n", response);
+		}
+
+		[Fact]
+		public void PingPingCommand_ReturnsPongPong()
+		{
+			var stream = GetNewClientStream();
+
+			var request = "*1\r\n$4\r\nPING\r\n";
+			
+			var response1 = SendRequestAndGetResponse(request, stream);
+			Assert.Equal("+PONG\r\n", response1);
+			
+			var response2 = SendRequestAndGetResponse(request, stream);
+			Assert.Equal("+PONG\r\n", response2);
+		}
+
+		private NetworkStream GetNewClientStream()
+		{
+			var newClient = new TcpClient(CLIENT_HOSTNAME, CLIENT_PORT);
+
+			return newClient.GetStream();
+		}
+
+		private string SendRequestAndGetResponse(string request, NetworkStream stream)
+		{
+			var requestBytes = Encoding.UTF8.GetBytes(request);
+			stream.Write(requestBytes);
+
+			var buffer = new byte[1024];
+			var bytesRead = stream.Read(buffer, 0, buffer.Length);
+			
+			return Encoding.UTF8.GetString(buffer, 0, bytesRead);
+		}
+	}
+}
